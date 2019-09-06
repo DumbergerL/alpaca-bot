e0dddf00c4daffbdaf79bcead19cb7ac4178850b
Lukas Dumberger <lukas.dumberger@dumbergerl.de>
2019-09-06 14:06:18 +0200

    setup other bots

--- start-bot.js
+++ start-bot.js
@@ -1,4 +1,16 @@
 var Vakahla = require('./lib/bot_vakahla');
+var Donald = require('./lib/bot_donald');
+var Jambalaya = require('./lib/bot_jambalaya');
 
-var theJohnBot = new Vakahla("John");
-theJohnBot.leavePoints = 2;
+
+var theJohnBot = new Jambalaya("John");
+
+var theLukasBot = new Jambalaya("Lukas");
+
+var theMustiBot = new Vakahla("Mustafa"); 
+theMustiBot.leavePoints = 3;
+
+var theJanBot = new Vakahla("Jan"); 
+theMustiBot.leavePoints = 3;
+
+
